ecf817c09bbc5ca4082faaf0cd5faf365347cf8f
WaqarKhatana220 <22100199@lums.edu.pk>
2025-02-02 22:47:59 +0500

    whitenoise configuration and staticfiles added to avoid running collecstatic on render

--- travel/settings.py
+++ travel/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
 os.path.join(BASE_DIR,"static")
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home2/media/media.lawrence.com/media/"
